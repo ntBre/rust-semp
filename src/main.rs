f0c655417ed2e9faa375e3f9ce83cf0f85f96c1e
Brent Westbrook <brentrwestbrook@gmail.com>
2022-03-25 10:54:28 -0500

    use libc dup2 for redirecting error and output files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::fs::File;
+use std::os::unix::io::AsRawFd;
+
 use rust_semp::slurm::Slurm;
 use rust_semp::*;
 
 fn main() {
     let names = vec!["C", "C", "C", "H", "H"];
+    let outfile = File::create("rust.out").expect("failed to create outfile");
+    let logfile = File::create("rust.log").expect("failed to create log file");
+    let out_fd = outfile.as_raw_fd();
+    let log_fd = logfile.as_raw_fd();
+    // redirect stdout to outfile and stderr to logfile
+    unsafe {
+        libc::dup2(out_fd, 1);
+        libc::dup2(log_fd, 2);
+    }
     run_algo(names, "file07", "params.dat", "rel.dat", Slurm::default());
 }

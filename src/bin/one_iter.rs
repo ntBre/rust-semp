3568d5508cd822756deacaf80c5359bbfbced0a7
Brent Westbrook <brentrwestbrook@gmail.com>
2022-05-25 17:22:09 -0500

    work on speeding up energy calculations, mostly in psqs

--- src/bin/one_iter.rs
+++ src/bin/one_iter.rs
@@ -9,10 +9,12 @@ fn main() {
     let params = load_params("test_files/params.dat");
     let mut jobs = build_jobs(&moles, &params, 0, 1.0, 0, 0);
     let mut energies = vec![0.; ml];
+    setup();
     LocalQueue {
         dir: "inp".to_string(),
     }
     .drain(&mut jobs, &mut energies);
+    takedown();
     for (i, e) in energies.iter().enumerate() {
         println!("{i:5}{e:20.12}");
     }

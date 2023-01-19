f4c50118d9e0d99944cb3628d1e494c734a0f12b
Brent Westbrook <brentrwestbrook@gmail.com>
2023-01-19 12:42:35 -0500

    also takedown opt directory created by normals

--- src/utils.rs
+++ src/utils.rs
@@ -95,7 +95,7 @@ pub fn load_params(filename: &str) -> Params {
     parse_params(&params)
 }
 
-pub(crate) static DIRS: &[&str] = &["inp", "tmparam"];
+pub(crate) static DIRS: &[&str] = &["inp", "tmparam", "opt"];
 
 /// set up the directories needed for the program after deleting existing ones
 pub fn setup() {

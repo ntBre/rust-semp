6e8e6084b4d2a73da27210623ca127ca3a09881e
Brent Westbrook <brentrwestbrook@gmail.com>
2023-06-21 19:57:31 -0400

    into_iter instead of cloning

--- src/optimize/frequency.rs
+++ src/optimize/frequency.rs
@@ -1026,8 +1026,7 @@ impl Optimize for Frequency {
             }
             // zip it with the FreqParts
             let pairs: Vec<_> = energy_chunks
-                .iter()
-                .cloned()
+                .into_iter()
                 .zip(freqs.pop().unwrap())
                 .collect();
             // iterate over the energy/freqparts pairs in parallel
